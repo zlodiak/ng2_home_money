c1be2327426fd39824ed6ab2e3af49fe661d8a10
zlodiak <prozaik81-2@yandex.ru>
2017-12-18 21:26:49 +0300

    validators ok

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -19,7 +19,7 @@ export class RegistrationComponent implements OnInit {
 
   ngOnInit() {
   	this.form = new FormGroup({
-  		'email': new FormControl(null, [Validators.required, Validators.email]),
+  		'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmail.bind(this)),
   		'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
   		'name': new FormControl(null, [Validators.required]),
   		'agree': new FormControl(false, [Validators.requiredTrue])
@@ -42,6 +42,17 @@ export class RegistrationComponent implements OnInit {
         nowCanLogin: true
       }});
     });
+  }
 
+  forbiddenEmail(control: FormControl): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.usersService.getUserByEmail(control.value).subscribe((user: User) => {
+        if(user) {
+          resolve({forbiddenEmail: true});
+        } else {
+          resolve(null);
+        }
+      });
+    });
   }
 }
