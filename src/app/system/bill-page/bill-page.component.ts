45dd389154d925bdf38a92a61e31a0da05dcd010
zlodiak <prozaik81-2@yandex.ru>
2017-12-19 15:26:50 +0300

    curr get

--- src/app/system/bill-page/bill-page.component.ts
+++ src/app/system/bill-page/bill-page.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+
+import { BillService } from '../shared/services/bill.service'
+import { Bill } from '../shared/models/bill.model'
+
 
 @Component({
   selector: 'wfm-bill-page',
   templateUrl: './bill-page.component.html',
   styleUrls: ['./bill-page.component.scss']
 })
-export class BillPageComponent implements OnInit {
+export class BillPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+	sub: Subscription;
+
+  constructor(private billService: BillService) { }
 
   ngOnInit() {
+  	this.sub = Observable.combineLatest(
+  		this.billService.getBill(),
+  		this.billService.getCurrency()
+  	).subscribe((data: [Bill, any]) => {
+  		console.log(data);
+  	})
+  }
+
+  ngOnDestroy() {
+  	this.sub.unsubscribe();
   }
 
 }
