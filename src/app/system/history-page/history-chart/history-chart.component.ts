aeefe8c09ce623ca86654cd651c9ccf5bfaba27d
zlodiak <prozaik81-2@yandex.ru>
2017-12-21 20:18:32 +0300

    before chart

---
+++ src/app/system/history-page/history-chart/history-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wfm-history-chart',
+  templateUrl: './history-chart.component.html',
+  styleUrls: ['./history-chart.component.scss']
+})
+export class HistoryChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
