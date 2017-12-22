a7037e19689f6d513ae898771d35613f5a85f54b
zlodiak <prozaik81-2@yandex.ru>
2017-12-22 04:59:44 +0300

    details ok

--- src/app/system/history-page/history-detail/history-detail.component.ts
+++ src/app/system/history-page/history-detail/history-detail.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+import { CategoriesService } from '../../shared/services/categories.service';
+import { EventService } from '../../shared/services/events.service';
+
+import { Category } from '../../shared/models/category.model';
+import { WFMEvent } from '../../shared/models/event.model';
 
 @Component({
   selector: 'wfm-history-detail',
   templateUrl: './history-detail.component.html',
   styleUrls: ['./history-detail.component.scss']
 })
-export class HistoryDetailComponent implements OnInit {
+export class HistoryDetailComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+	event: WFMEvent;
+	category: Category;
+
+	isLoaded = false;
+
+	s1: Subscription;
+
+  constructor(private route: ActivatedRoute, 
+  						private eventsService: EventService, 
+  						private categoriesService: CategoriesService) { }
 
   ngOnInit() {
+  	this.s1 = this.route.params
+  		.mergeMap((params: Params) => this.eventsService.getEventById(params['id']) )
+  		.mergeMap((event: WFMEvent) => {
+  			this.event = event;
+  			return this.categoriesService.getCategoryById(event.category);
+  		})
+	  	.subscribe((category: Category) => {
+	  		this.category = category;
+	  		this.isLoaded = true;
+	  	});
+  }
+
+  ngOnDestroy() {
+  	if(this.s1) this.s1.unsubscribe();
   }
 
 }
